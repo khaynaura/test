e8be23b0ad9f3a92765359bd26e2892105fc864c
khaynaura <146971893+khaynaura@users.noreply.github.com>
2024-10-24 23:55:53 +0700

    anjai

--- product/apps.py
+++ product/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class ProductConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "product"
+
+    def ready(self):
+        import sys
+        if 'runserver' in sys.argv:
+            try:
+                print("Loading fixture data...")
+                call_command('loaddata', 'fixtures/products.json')
+            except Exception as e:
+                print(f"Error loading fixture: {e}")
+
+#
